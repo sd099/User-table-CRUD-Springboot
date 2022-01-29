a86c8b8465d6f5ed09c456818e3f1a11a9588552
unknown <sdas680.sd@gmail.com>
2022-01-29 23:17:16 +0530

    pom.xml and UserApiApp file setup

---
+++ src/main/java/org/paytm/milestones1/UserApiApp.java
@@ -0,0 +1,12 @@
+package org.paytm.milestones1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApiApp {
+
+    public static void main(String[] args){
+        SpringApplication.run(UserApiApp.class,args);
+    }
+}
